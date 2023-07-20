29e11a4c8eee9b322c93f2334861cfe48396a583
traynham <jesse@traynham.com>
2023-07-20 18:38:10 -0500

    Initial commit.

---
+++ tests/__tests__/03 merge.js
@@ -0,0 +1,27 @@
+import { describe, expect, beforeEach, it } from '@jest/globals'
+
+import Lorry from '../../src/lorry.js'
+
+describe('Merge', () => {
+
+	let lorry
+  
+	beforeEach(() => {
+		lorry = new Lorry()
+	})
+
+	it('should merge correctly', () => {
+		lorry.Merge({key1: 'value1'})
+		expect(lorry.key1).toBe('value1')
+		lorry.Merge({key2: 'value2'})
+		expect(lorry.key2).toBe('value2')
+	})
+
+	it('should not overwrite class methods when merging with conflicting keys', () => {
+		const obj = { Flash: 'conflict', Merge: 'conflict', Message: 'conflict' }
+		lorry.Merge(obj)
+		expect(typeof lorry.Flash).toBe('function')
+		expect(typeof lorry.Merge).toBe('function')
+	})
+	
+})
